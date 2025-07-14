1235fe57cbf042a085bbdfedf9c088eebeb746b5
Adzahari <wnabwt1992@gmail.com>
2025-07-14 23:25:31 +0800

    feat: update layout structure for improved responsiveness and adjust main content height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { MortgageTermsForm } from "@/components/mortgage-terms-form"
 
 export default function Home() {
 	return (
-		<div className="flex flex-col lg:flex-row h-full space-y-4 lg:space-y-0 lg:space-x-1 p-1 sm:p-1 lg:p-1">
+		<div className="flex flex-col lg:flex-row min-h-full space-y-4 lg:space-y-0 lg:space-x-1 p-1 sm:p-1 lg:p-1">
 			<div className="w-full lg:w-auto lg:flex-shrink-0">
 				<MortgageTermsForm />
 			</div>
